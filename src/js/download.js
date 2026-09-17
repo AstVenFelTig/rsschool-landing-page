65210992423ed74440304a03e83db70d3e0a81a5
AstVenFelTig <Alex-Sadr1092@yandex.ru>
2026-09-17 19:12:01 +0300

    feat: add download

---
+++ src/js/download.js
@@ -0,0 +1,18 @@
+export default function linkApp() {
+  const downloadApple = document.querySelectorAll(".download__apple");
+  console.log(downloadApple[0]);
+  downloadApple[0].addEventListener("click", () => {
+    window.open(
+      "https://www.apple.com/apple-pay/",
+      "_blank",
+      "noopener,noreferrer",
+    );
+  });
+  downloadApple[1].addEventListener("click", () => {
+    window.open(
+      "https://play.google.com/store/apps?hl=ru",
+      "_blank",
+      "noopener,noreferrer",
+    );
+  });
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import "inter-ui/inter.css";
 import imgHeaderLogo from "./assets/logo.png";
 import thems from "./js/thems.js";
 import slider from "./js/slider.js";
-
+import linkApp from "./js/download.js";
 const headerLogo = document.querySelector(".header__logo");
 headerLogo.src = imgHeaderLogo;
 thems();
 slider();
+linkApp();
