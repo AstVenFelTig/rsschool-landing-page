6a5c27a8cf2050043a48c090a4355e3fd5edf21a
AstVenFelTig <Alex-Sadr1092@yandex.ru>
2026-09-20 18:15:19 +0300

    feat: add catalog

--- src/catalog.js
+++ src/catalog.js
@@ -2,6 +2,8 @@ import "./style/main.scss";
 import "inter-ui/inter.css";
 import thems from "./js/thems-catalog.js";
 import imgHeaderLogo from "./assets/logo.png";
+import cards from "./js/cards.js";
 const headerLogo = document.querySelector(".header__logo");
 headerLogo.src = imgHeaderLogo;
 thems();
+cards();
